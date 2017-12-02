24d126ba7964fd4b46baee0d7a23d994abce1b7f
MikeMakes <miglopgil@alum.us.es>
2017-12-03 00:10:10 +0100

    Not so late as I would say

--- pipe_hitbox.vhd
+++ pipe_hitbox.vhd
@@ -1,108 +1,52 @@
---------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
 -- Company: 
--- Engineer:
+-- Engineer: 
+-- 
+-- Create Date:    15:59:38 12/02/2017 
+-- Design Name: 
+-- Module Name:    pipe_hitbox - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
 --
--- Create Date:   13:49:19 12/02/2017
--- Design Name:   
--- Module Name:   /home/usuario/Floppy_Bird/pipe_hitbox.vhd
--- Project Name:  Floppy_Bird
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: floppy_hitbox
--- 
--- Dependencies:
--- 
--- Revision:
+-- Dependencies: 
+--
+-- Revision: 
 -- Revision 0.01 - File Created
--- Additional Comments:
+-- Additional Comments: 
 --
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
- 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
-ENTITY pipe_hitbox IS
-END pipe_hitbox;
- 
-ARCHITECTURE behavior OF pipe_hitbox IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT floppy_hitbox
-    PORT(
-         clk : IN  std_logic;
-         rst : IN  std_logic;
-         en : IN  std_logic;
-         floppy_v : IN  std_logic_vector(9 downto 0);
-         floppy_pos_x : OUT  std_logic_vector(9 downto 0);
-         floppy_pos_y : OUT  std_logic_vector(9 downto 0);
-         floppy_right : OUT  std_logic_vector(9 downto 0);
-         floppy_top : OUT  std_logic_vector(9 downto 0)
-        );
-    END COMPONENT;
-    
+--use IEEE.NUMERIC_STD.ALL;
 
-   --Inputs
-   signal clk : std_logic := '0';
-   signal rst : std_logic := '0';
-   signal en : std_logic := '0';
-   signal floppy_v : std_logic_vector(9 downto 0) := (others => '0');
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
- 	--Outputs
-   signal floppy_pos_x : std_logic_vector(9 downto 0);
-   signal floppy_pos_y : std_logic_vector(9 downto 0);
-   signal floppy_right : std_logic_vector(9 downto 0);
-   signal floppy_top : std_logic_vector(9 downto 0);
+entity pipe_hitbox is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           en : in  STD_LOGIC;
+           x : in  STD_LOGIC;
+           y : in  STD_LOGIC;
+           unnamed : in  STD_LOGIC;
+           pipe_pos_x : out  STD_LOGIC;
+           pipe_right : out  STD_LOGIC;
+           pipe_pos_y : out  STD_LOGIC;
+           pipe_top : out  STD_LOGIC;
+           inside_pipe : out  STD_LOGIC);
+end pipe_hitbox;
 
-   -- Clock period definitions
-   constant clk_period : time := 10 ns;
- 
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: floppy_hitbox PORT MAP (
-          clk => clk,
-          rst => rst,
-          en => en,
-          floppy_v => floppy_v,
-          floppy_pos_x => floppy_pos_x,
-          floppy_pos_y => floppy_pos_y,
-          floppy_right => floppy_right,
-          floppy_top => floppy_top
-        );
+architecture Behavioral of pipe_hitbox is
 
-   -- Clock process definitions
-   clk_process :process
-   begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
-   end process;
- 
+begin
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;	
 
-      wait for clk_period*10;
+end Behavioral;
 
-      -- insert stimulus here 
-
-      wait;
-   end process;
-
-END;
