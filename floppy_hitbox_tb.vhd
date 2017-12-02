f3020395931d034004f0e6af85c136a19addd526
MikeMakes <miglopgil@alum.us.es>
2017-12-02 15:30:53 +0100

    Hitbox now knows if it should be being draw. for real

--- floppy_hitbox_tb.vhd
+++ floppy_hitbox_tb.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   23:31:00 12/01/2017
+-- Create Date:   14:32:34 12/02/2017
 -- Design Name:   
--- Module Name:   /home/usuario/Floppy_Bird/floppy_bird_tb.vhd
+-- Module Name:   /home/usuario/Floppy_Bird/floppy_hitbox_tb.vhd
 -- Project Name:  Floppy_Bird
 -- Target Device:  
 -- Tool versions:  
@@ -32,10 +32,10 @@ USE ieee.std_logic_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 USE ieee.numeric_std.ALL;
  
-ENTITY floppy_bird_tb IS
-END floppy_bird_tb;
+ENTITY floppy_hitbox_tb IS
+END floppy_hitbox_tb;
  
-ARCHITECTURE behavior OF floppy_bird_tb IS 
+ARCHITECTURE behavior OF floppy_hitbox_tb IS 
  
     -- Component Declaration for the Unit Under Test (UUT)
  
@@ -44,11 +44,14 @@ ARCHITECTURE behavior OF floppy_bird_tb IS
          clk : IN  std_logic;
          rst : IN  std_logic;
          en : IN  std_logic;
+         x : IN  unsigned(9 downto 0);
+         y : IN  unsigned(9 downto 0);
          floppy_v : IN  unsigned(9 downto 0);
          floppy_pos_x : OUT  unsigned(9 downto 0);
          floppy_pos_y : OUT  unsigned(9 downto 0);
          floppy_right : OUT  unsigned(9 downto 0);
-         floppy_top : OUT  unsigned(9 downto 0)
+         floppy_top : OUT  unsigned(9 downto 0);
+         inside_floppy : OUT  std_logic
         );
     END COMPONENT;
     
@@ -57,6 +60,8 @@ ARCHITECTURE behavior OF floppy_bird_tb IS
    signal clk : std_logic := '0';
    signal rst : std_logic := '0';
    signal en : std_logic := '0';
+   signal x : unsigned(9 downto 0) := (others => '0');
+   signal y : unsigned(9 downto 0) := (others => '0');
    signal floppy_v : unsigned(9 downto 0) := (others => '0');
 
  	--Outputs
@@ -64,6 +69,7 @@ ARCHITECTURE behavior OF floppy_bird_tb IS
    signal floppy_pos_y : unsigned(9 downto 0);
    signal floppy_right : unsigned(9 downto 0);
    signal floppy_top : unsigned(9 downto 0);
+   signal inside_floppy : std_logic;
 
    -- Clock period definitions
    constant clk_period : time := 10 ns;
@@ -75,11 +81,14 @@ BEGIN
           clk => clk,
           rst => rst,
           en => en,
+          x => x,
+          y => y,
           floppy_v => floppy_v,
           floppy_pos_x => floppy_pos_x,
           floppy_pos_y => floppy_pos_y,
           floppy_right => floppy_right,
-          floppy_top => floppy_top
+          floppy_top => floppy_top,
+          inside_floppy => inside_floppy
         );
 
    -- Clock process definitions
@@ -98,15 +107,19 @@ BEGIN
       -- hold reset state for 100 ns.
       wait for 100 ns;	
 
-      wait for clk_period*10;
+      wait for clk_period*20;
 
       -- insert stimulus here 
 		rst<='1';
 		en<='1';
+		x<="0000000000";
+		y<="0000000000";
 		floppy_v<="0000010100";
 		
 		wait for clk_period*10;
 		rst<='0';
+		x<=to_unsigned(79,10);
+		y<=to_unsigned(300,10);
 		
 		wait for clk_period*10;
 		floppy_v<="0000001010";
